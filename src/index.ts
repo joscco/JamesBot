0f3cf17867714d62f97332655de4f38be9d480b1
Jonathan Schmitz <jomitz96@hotmail.com>
2023-12-29 23:35:46 +0100

    Update Bot Structure

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,16 @@
-import {Context, Markup, Scenes, session, Telegraf} from "telegraf";
-import {JamesTaskRepo} from "./infrastructure/JamesTaskRepo";
+import {James} from "./James";
 import {BirthdayReminder} from "./Reminders/BirthdayReminder";
+import {JamesTaskRepo} from "./infrastructure/JamesTaskRepo";
 import {GarbageReminder} from "./Reminders/GarbageReminder";
-import {ShowNextGarbages} from "./Garbage/ShowNextGarbages";
-import {ShowNextBirthdays} from "./Birthdays/ShowNextBirthdays";
-import {DeleteGarbage} from "./Garbage/DeleteGarbage";
-import {DeleteBirthday} from "./Birthdays/DeleteBirthday";
-import {AddPeriodicGarbage} from "./Garbage/AddPeriodicGarbage";
-import {IdChecker} from "./IdChecker";
-import {DeleteAllGarbages} from "./Garbage/DeleteAllGarbages";
-import {AddGarbage} from "./Garbage/AddGarbage";
-import {AddBirthday} from "./Birthdays/AddBirthday";
 
-export interface JamesContext extends Context {
-    scene: Scenes.SceneContextScene<JamesContext, Scenes.WizardSessionData>;
-}
-
-export interface JamesContextWithSession<T extends Scenes.WizardSessionData> extends JamesContext {
-    session: Scenes.WizardSession<T>;
-    scene: Scenes.SceneContextScene<JamesContextWithSession<T>, T>;
-    wizard: Scenes.WizardContextWizard<JamesContextWithSession<T>>;
-}
-
-const botToken = process.env.BOT_TOKEN;
 const jamesRepo = new JamesTaskRepo()
-
-if (botToken === undefined) {
-    throw Error("BOT_TOKEN must be defined!");
-}
-
-const garbageSubScenes = [
-    new AddGarbage(jamesRepo),
-    new AddPeriodicGarbage(jamesRepo),
-    new ShowNextGarbages(jamesRepo),
-    new DeleteGarbage(jamesRepo),
-    new DeleteAllGarbages(jamesRepo)
-]
-const garbageScene = new Scenes.BaseScene<JamesContext>('GARBAGE_SCENE')
-garbageScene.enter(Telegraf.reply('Müll also. Was willst du tun?',
-            Markup.keyboard([
-                ...garbageSubScenes.map(scene => [scene.getOptionTitle()]),
-                ["Abbrechen"]
-            ])
-                .oneTime()
-                .resize())
-);
-
-for (let garbageCommand of garbageSubScenes) {
-    garbageScene.hears(
-        garbageCommand.getOptionTitle(),
-        ctx => ctx.scene.enter(garbageCommand.getId()))
-}
-
-const birthdaySubScenes = [
-    new AddBirthday(jamesRepo),
-    new ShowNextBirthdays(jamesRepo),
-    new DeleteBirthday(jamesRepo)
-]
-
-const birthdayScene = new Scenes.BaseScene<JamesContext>('BIRTHDAY_SCENE')
-birthdayScene.enter(Telegraf.reply('Geburtstage also. Was willst du tun?',
-    Markup.keyboard([
-        ...birthdaySubScenes.map(scene => [scene.getOptionTitle()]),
-        ["Abbrechen"]
-    ])
-        .oneTime()
-        .resize())
-);
-for (let birthdayCommand of birthdaySubScenes) {
-    birthdayScene.hears(
-        birthdayCommand.getOptionTitle(),
-        ctx => ctx.scene.enter(birthdayCommand.getId()))
-}
-
-const bot = new Telegraf<JamesContext>(botToken, {
-    telegram: {
-        webhookReply: true
-    }
-});
-
-const stage = new Scenes.Stage<JamesContext>([
-    birthdayScene,
-    garbageScene,
-    ...birthdaySubScenes.map(scene => scene.getWizard()),
-    ...garbageSubScenes.map(scene => scene.getWizard())
-], {})
-
-bot.use(session())
-bot.use(stage.middleware())
-
-bot.hears('Müll', ctx => {
-        if (IdChecker.isValidID(ctx.message.from.id.toString())) {
-            return ctx.scene.enter('GARBAGE_SCENE')
-        } else {
-            ctx.reply("Dir gehorche ich nicht.");
-        }
-});
-
-bot.hears('Geburtstage', ctx => {
-    if (IdChecker.isValidID(ctx.message.from.id.toString())) {
-        return ctx.scene.enter('BIRTHDAY_SCENE')
-    } else {
-        ctx.reply("Dir gehorche ich nicht.");
-    }
-});
-bot.hears('Abbrechen', ctx => ctx.scene.leave())
-
-bot.on('message', Telegraf.reply("Womit kann ich helfen?",
-    Markup.keyboard([
-        ["Müll"],
-        ["Geburtstage"],
-        ["Abbrechen"]
-    ])
-        .oneTime()
-        .resize())
-)
+const james = new James(jamesRepo)
 
 // Hier muss die webhook-Option eingefügt werden, sonst wird der Webhook immer wieder auf null gesetzt!
 // Main Lambda function
 exports.mainHandler = async function (event) {
     try {
-        await bot.launch({
+        await james.bot.launch({
             webhook: {
                 domain: process.env.BOT_WEBHOOK_DOMAIN,
                 hookPath: process.env.BOT_WEBHOOK_PATH
@@ -130,7 +20,7 @@ exports.mainHandler = async function (event) {
         const body = event.body;
         console.log("Event stringly: " + JSON.stringify(event));
         // Body kommt als json Objekt rein, muss nicht geparsed werden.
-        await bot.handleUpdate(body);
+        await james.bot.handleUpdate(body);
     } catch (err) {
         console.log("Upps, ein Fehler ist aufgetreten: " + err)
     }
@@ -140,7 +30,7 @@ exports.mainHandler = async function (event) {
 exports.birthdayTriggerHandler = async function () {
     try {
         console.log("Führe Birthday Reminder aus.");
-        await new BirthdayReminder(jamesRepo).sendDailyBirthdayReminder(bot);
+        await new BirthdayReminder(jamesRepo).sendDailyBirthdayReminder(james.bot);
         console.log("Birthday Reminder ausgeführt.");
     } catch (err) {
         console.log("Upps, ein Fehler ist aufgetreten: " + err)
@@ -151,7 +41,7 @@ exports.birthdayTriggerHandler = async function () {
 exports.garbageTriggerHandler = async function () {
     try {
         console.log("Führe Garbage Reminder aus.");
-        await new GarbageReminder(jamesRepo).sendDailyGarbageReminder(bot);
+        await new GarbageReminder(jamesRepo).sendDailyGarbageReminder(james.bot);
         console.log("Garbage Reminder ausgeführt.");
     } catch (err) {
         console.log("Upps, ein Fehler ist aufgetreten: " + err)
